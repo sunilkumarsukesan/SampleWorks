78232d9a9b1e5d767e197a0a745751e7bee4f141
sunil_sukesan <sunil_sukesan@sunil>
2019-06-22 19:43:58 +0530

    Webtables updated

--- src/test/java/edu/training/testingPrograms/WebTables.java
+++ src/test/java/edu/training/testingPrograms/WebTables.java
@@ -2,6 +2,7 @@ package edu.training.testingPrograms;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -13,8 +14,14 @@ public class WebTables {
 		RemoteWebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
-		driver.get("https://www.railyatri.in/time-table/12626-new-delhi-trivandrum-kerala-sf-express");
-		
+		driver.get("https://www.railyatri.in/time-table");
+		String firstText = driver.findElement(By.xpath("//table[@class=\"table table-condensed\"]//tr[1]/th[1]")).getText();
+		driver.close();
+		if(!firstText.isEmpty())
+			System.out.println("First text in the webtable is : " + firstText);
+		else
+			System.out.println("Unable to retrieve the firsttext in the webtable");
+
 	}
 
 }
